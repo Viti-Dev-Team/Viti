ca6e07fba96547041e1e2a37de868ecf9286f86a
Isax82 <isaieroelandt64@gmail.com>
2026-02-24 09:30:36 +0100

    Source code

--- src/viti.py
+++ src/viti.py
@@ -2,32 +2,39 @@ import sys
 import json
 import os
 
-from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QToolBar,
-    QAction, QLineEdit, QTabWidget,
-    QShortcut, QMenu, QMessageBox, QWidget,
-    QVBoxLayout, QListWidget, QListWidgetItem
+    QApplication,
+    QMainWindow,
+    QToolBar,
+    QAction,
+    QLineEdit,
+    QTabWidget,
+    QShortcut,
+    QMenu,
+    QMessageBox,
 )
 from PyQt5.QtGui import QKeySequence
 from PyQt5.QtWebEngineWidgets import (
     QWebEngineView,
     QWebEngineProfile,
-    QWebEnginePage
+    QWebEnginePage,
 )
-
 
 BOOKMARK_FILE = "bookmarks.json"
 
 
 class BrowserTab(QWebEngineView):
-    def __init__(self, profile: QWebEngineProfile):
+    def __init__(self, profile: QWebEngineProfile, url: str):
         super().__init__()
 
         page = QWebEnginePage(profile, self)
         self.setPage(page)
 
-        self.setUrl(QUrl("https://www.google.com"))
+        if not url:
+            url = "https://www.google.com"
+
+        self.setUrl(QUrl(str(url)))
 
 
 class VitiBrowser(QMainWindow):
@@ -35,7 +42,7 @@ class VitiBrowser(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Viti Browser")
-        self.resize(1500, 900)
+        self.resize(1400, 800)
 
         self.profile = QWebEngineProfile.defaultProfile()
 
@@ -43,7 +50,7 @@ class VitiBrowser(QMainWindow):
         self.load_bookmarks()
 
         self.init_ui()
-        self.add_new_tab()
+        self.add_new_tab("https://www.google.com")
 
     def init_ui(self):
 
@@ -51,7 +58,6 @@ class VitiBrowser(QMainWindow):
         self.tabs.setTabsClosable(True)
         self.tabs.setMovable(True)
         self.tabs.setDocumentMode(True)
-
         self.tabs.tabCloseRequested.connect(self.close_tab)
         self.tabs.currentChanged.connect(self.sync_url_bar)
 
@@ -62,19 +68,19 @@ class VitiBrowser(QMainWindow):
         self.addToolBar(toolbar)
 
         back = QAction("◀", self)
-        back.triggered.connect(lambda: self.current_browser().back())
+        back.triggered.connect(self.go_back)
         toolbar.addAction(back)
 
         forward = QAction("▶", self)
-        forward.triggered.connect(lambda: self.current_browser().forward())
+        forward.triggered.connect(self.go_forward)
         toolbar.addAction(forward)
 
-        reload = QAction("⟳", self)
-        reload.triggered.connect(lambda: self.current_browser().reload())
-        toolbar.addAction(reload)
+        reload_btn = QAction("⟳", self)
+        reload_btn.triggered.connect(self.reload_page)
+        toolbar.addAction(reload_btn)
 
         self.url_bar = QLineEdit()
-        self.url_bar.setPlaceholderText("Rechercher ou entrer une URL...")
+        self.url_bar.setPlaceholderText("Search or enter URL")
         self.url_bar.returnPressed.connect(self.navigate)
         toolbar.addWidget(self.url_bar)
 
@@ -82,16 +88,145 @@ class VitiBrowser(QMainWindow):
         bookmark_btn.triggered.connect(self.add_bookmark)
         toolbar.addAction(bookmark_btn)
 
-        new_tab = QAction("➕", self)
-        new_tab.triggered.connect(self.add_new_tab)
-        toolbar.addAction(new_tab)
+        new_tab_btn = QAction("➕", self)
+        new_tab_btn.triggered.connect(lambda: self.add_new_tab("https://www.google.com"))
+        toolbar.addAction(new_tab_btn)
 
-        QShortcut(QKeySequence("Ctrl+T"), self, activated=self.add_new_tab)
+        QShortcut(QKeySequence("Ctrl+T"), self,
+                  activated=lambda: self.add_new_tab("https://www.google.com"))
 
-        self.bookmarks_menu = self.menuBar().addMenu("Favoris")
+        self.bookmarks_menu = self.menuBar().addMenu("Bookmarks")
         self.refresh_bookmarks_menu()
 
         self.apply_style()
+
+    def add_new_tab(self, url: str):
+
+        if not url:
+            url = "https://www.google.com"
+
+        browser = BrowserTab(self.profile, url)
+
+        index = self.tabs.addTab(browser, "Loading...")
+        self.tabs.setCurrentIndex(index)
+
+        browser.loadFinished.connect(
+            lambda _, i=index, b=browser:
+            self.tabs.setTabText(i, b.page().title())
+        )
+
+        browser.urlChanged.connect(
+            lambda q, b=browser:
+            self.update_url_bar(q)
+        )
+
+    def close_tab(self, index):
+        if self.tabs.count() > 1:
+            self.tabs.removeTab(index)
+
+    def current_browser(self):
+        return self.tabs.currentWidget()
+
+    def navigate(self):
+
+        text = self.url_bar.text()
+
+        if "." not in text:
+            url = "https://www.google.com/search?q=" + text
+        elif not text.startswith("http"):
+            url = "https://" + text
+        else:
+            url = text
+
+        self.current_browser().setUrl(QUrl(url))
+
+    def go_back(self):
+        browser = self.current_browser()
+        if browser:
+            browser.back()
+
+    def go_forward(self):
+        browser = self.current_browser()
+        if browser:
+            browser.forward()
+
+    def reload_page(self):
+        browser = self.current_browser()
+        if browser:
+            browser.reload()
+
+    def update_url_bar(self, qurl):
+        self.url_bar.setText(qurl.toString())
+
+    def sync_url_bar(self):
+        browser = self.current_browser()
+        if browser:
+            self.url_bar.setText(browser.url().toString())
+
+    def add_bookmark(self):
+
+        browser = self.current_browser()
+        if not browser:
+            return
+
+        title = browser.page().title()
+        url = browser.url().toString()
+
+        self.bookmarks.append({"title": title, "url": url})
+        self.save_bookmarks()
+        self.refresh_bookmarks_menu()
+
+        QMessageBox.information(self, "Bookmark", "Added to bookmarks")
+
+    def delete_bookmark(self, index):
+
+        reply = QMessageBox.question(
+            self,
+            "Delete Bookmark",
+            "Delete this bookmark?",
+            QMessageBox.Yes | QMessageBox.No,
+        )
+
+        if reply == QMessageBox.Yes:
+            self.bookmarks.pop(index)
+            self.save_bookmarks()
+            self.refresh_bookmarks_menu()
+
+    def load_bookmarks(self):
+        if os.path.exists(BOOKMARK_FILE):
+            with open(BOOKMARK_FILE, "r", encoding="utf-8") as f:
+                self.bookmarks = json.load(f)
+        else:
+            self.bookmarks = []
+
+    def save_bookmarks(self):
+        with open(BOOKMARK_FILE, "w", encoding="utf-8") as f:
+            json.dump(self.bookmarks, f, indent=4)
+
+    def refresh_bookmarks_menu(self):
+
+        self.bookmarks_menu.clear()
+
+        for index, bookmark in enumerate(self.bookmarks):
+
+            submenu = QMenu(bookmark["title"], self)
+
+            open_action = QAction("🌍 Open", self)
+            open_action.triggered.connect(
+                lambda _, url=bookmark["url"]:
+                self.add_new_tab(url)
+            )
+
+            delete_action = QAction("❌ Delete", self)
+            delete_action.triggered.connect(
+                lambda _, idx=index:
+                self.delete_bookmark(idx)
+            )
+
+            submenu.addAction(open_action)
+            submenu.addAction(delete_action)
+
+            self.bookmarks_menu.addMenu(submenu)
 
     def apply_style(self):
         self.setStyleSheet("""
@@ -123,119 +258,6 @@ class VitiBrowser(QMainWindow):
                 background: #35363a;
             }
         """)
-
-    def add_new_tab(self, url=None):
-
-        if url is None:
-            url = QUrl("https://www.google.com")
-
-        browser = BrowserTab(self.profile)
-        browser.setUrl(url)
-
-        index = self.tabs.addTab(browser, "Nouvel onglet")
-        self.tabs.setCurrentIndex(index)
-
-        browser.loadFinished.connect(
-            lambda _, i=index, b=browser:
-            self.tabs.setTabText(i, b.page().title())
-        )
-
-        browser.urlChanged.connect(
-            lambda q, b=browser:
-            self.update_url_bar(q, b)
-        )
-
-    def close_tab(self, index):
-        if self.tabs.count() > 1:
-            self.tabs.removeTab(index)
-
-    def current_browser(self):
-        return self.tabs.currentWidget()
-
-    def sync_url_bar(self):
-        browser = self.current_browser()
-        if browser:
-            self.url_bar.setText(browser.url().toString())
-
-    def navigate(self):
-
-        url = self.url_bar.text()
-
-        if "." not in url:
-            url = "https://www.google.com/search?q=" + url
-        elif not url.startswith("http"):
-            url = "https://" + url
-
-        self.current_browser().setUrl(QUrl(url))
-
-    def update_url_bar(self, qurl, browser):
-        if browser == self.current_browser():
-            self.url_bar.setText(qurl.toString())
-
-    def add_bookmark(self):
-
-        browser = self.current_browser()
-        title = browser.page().title()
-        url = browser.url().toString()
-
-        self.bookmarks.append({"title": title, "url": url})
-
-        self.save_bookmarks()
-        self.refresh_bookmarks_menu()
-
-        QMessageBox.information(self, "Favori", "Ajouté ⭐")
-
-    def delete_bookmark(self, index):
-
-        reply = QMessageBox.question(
-            self,
-            "Supprimer",
-            "Supprimer ce favori ?",
-            QMessageBox.Yes | QMessageBox.No
-        )
-
-        if reply == QMessageBox.Yes:
-            self.bookmarks.pop(index)
-            self.save_bookmarks()
-            self.refresh_bookmarks_menu()
-
-    def load_bookmarks(self):
-
-        if os.path.exists(BOOKMARK_FILE):
-            with open(BOOKMARK_FILE, "r", encoding="utf-8") as f:
-                self.bookmarks = json.load(f)
-        else:
-            self.bookmarks = []
-
-    def save_bookmarks(self):
-
-        with open(BOOKMARK_FILE, "w", encoding="utf-8") as f:
-            json.dump(self.bookmarks, f, indent=4)
-
-    def refresh_bookmarks_menu(self):
-
-        self.bookmarks_menu.clear()
-
-        for index, bookmark in enumerate(self.bookmarks):
-
-            submenu = QMenu(bookmark["title"], self)
-
-            open_action = QAction("🌍 Ouvrir", self)
-            open_action.triggered.connect(
-                lambda _, url=bookmark["url"]:
-                self.add_new_tab(QUrl(url))
-            )
-
-            delete_action = QAction("❌ Supprimer", self)
-            delete_action.triggered.connect(
-                lambda _, idx=index:
-                self.delete_bookmark(idx)
-            )
-
-            submenu.addAction(open_action)
-            submenu.addAction(delete_action)
-
-            self.bookmarks_menu.addMenu(submenu)
 
 
 if __name__ == "__main__":
